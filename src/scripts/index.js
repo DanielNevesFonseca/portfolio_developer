8cda40f67e896ac06eea0dbec5824d84a370d8d2
Daniel Neves Fonseca <daniel.neves.fonseca.ti@gmail.com>
2025-04-27 17:55:10 -0300

    feat: Improve mobile menu functionality and add smooth scrolling behavior

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,21 @@
 function toggleMenu() {
-  const mobileMenu = document.getElementById('mobileMenu');
-  mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
+  const mobileMenu = document.getElementById("mobileMenu");
+  mobileMenu.style.display =
+    mobileMenu.style.display === "flex" ? "none" : "flex";
+
+  if (mobileMenu.style.display === "flex") {
+    document.addEventListener("click", closeMenuOnClickOutside);
+  } else {
+    document.removeEventListener("click", closeMenuOnClickOutside);
+  }
+}
+
+function closeMenuOnClickOutside(event) {
+  const mobileMenu = document.getElementById("mobileMenu");
+  const hamburger = document.querySelector(".hamburger");
+
+  if (!mobileMenu.contains(event.target) && !hamburger.contains(event.target)) {
+    mobileMenu.style.display = "none";
+    document.removeEventListener("click", closeMenuOnClickOutside);
+  }
 }
